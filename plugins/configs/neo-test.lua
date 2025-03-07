08fb4596cc42da955360685ad63b8f32e3de3546
Toddneal Stallworth <43554061+Moriango@users.noreply.github.com>
2025-03-06 20:35:39 -0800

    Add files via upload

---
+++ plugins/configs/neo-test.lua
@@ -0,0 +1,10 @@
+--- @type NvPluginSpec
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter"
+  }
+}
---
+++ plugins/configs/tmux-navigator.lua
@@ -0,0 +1,11 @@
+---@type NvPluginSpec
+return{
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+}
