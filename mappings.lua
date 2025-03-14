4ea89cdcf377b9907c5747d9cb184ffe5c4a6afa
Toddneal Stallworth <43554061+Moriango@users.noreply.github.com>
2025-03-14 07:59:27 -0700

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -208,7 +208,7 @@ map("n", "<leader>cf", function()
         print("Error counting files")
     end
 end, { desc = "Count files in current directory", noremap = true })
-
+-- Amazon Specific; finds the package name after the src file and copies the name to the clipboard
 map("n", "yp", function()
     local filepath = vim.fn.expand("%:p")  -- Get the full path of current file
     local after_src = filepath:match("src/(.-)/")  -- Match the first folder after src/
