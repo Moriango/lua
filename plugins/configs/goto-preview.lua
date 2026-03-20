f1f60315a355f68d8367e99144672829d47ea383
Moriango <toddnealstallworth@gmail.com>
2026-03-20 14:46:03 -0400

    Updated goto preview

--- plugins/configs/goto-preview.lua
+++ plugins/configs/goto-preview.lua
@@ -22,9 +22,9 @@ return {
     }
 
     -- Optional: Set up custom key mappings
-    vim.api.nvim_set_keymap('n', 'gpd', "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', 'gpi', "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', 'gpr', "<cmd>lua require('goto-preview').goto_preview_references()<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', 'px', "<cmd>lua require('goto-preview').close_all_win()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gpd', "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", { desc = "Go to Preview Definition", noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gpi', "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>", { desc = "Go to Preview Implementation", noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gpr', "<cmd>lua require('goto-preview').goto_preview_references()<CR>", { desc = "Go to Preview References", noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', 'px', "<cmd>lua require('goto-preview').close_all_win()<CR>", { desc = "Close preview window", noremap = true, silent = true })
   end,
 }
