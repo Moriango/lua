f70fe9931097500493786acd846db8483928fa93
moriango <toddnealstallworth@gmail.com>
2026-09-21 18:52:19 -0400

    removed zen mode shortcuts

--- plugins/configs/zen-mode.lua
+++ plugins/configs/zen-mode.lua
@@ -4,9 +4,6 @@ return {
   dependencies = { "folke/twilight.nvim" },
   cmd = "ZenMode",
   init = function()
-    local map = vim.keymap.set
-    map("n", "<leader>kz", "<cmd>ZenMode<CR>", { desc = "Toggles ZenMode"})
-
     -- Function to toggle Twilight
     local twilight_enabled = true
     function ToggleTwilight()
@@ -22,9 +19,6 @@ return {
 
     -- Create a command to toggle Twilight
     vim.api.nvim_create_user_command("ToggleTwilight", ToggleTwilight, {})
-
-    -- Optionally, create a keybinding to toggle Twilight
-    map("n", "kt", ":Twilight<CR>", { desc = "Toggles Twilight", noremap = true, silent = true })
   end,
   config = function()
   end,
